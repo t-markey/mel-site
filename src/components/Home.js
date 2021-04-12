168654ba7f9c1f94386429397cbcf17803c8381e
tom_m <t.markey@mailfence.com>
2021-04-12 19:55:59 -0300

    finish homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,21 @@
-import React from "react"
+import React from "react";
+import image from "../plant.jpg";
 
 export default function Home(){
-    return <h1>Home</h1>
+    return (
+        <main>
+            <img src={image} alt="Plant Leaves" className= "absolute object-cover w-full h-full"  />
+            <section className ="relative min-h-screen pt-9 lg:pt-33 px-10 py-10">
+                <h1 className ="text-lg text-black-200 font-bold cursive leading-none lg:leading-snug home-name "> Writer , Editor</h1>
+                <h5 className ="text-sm text-green-400 leading-none lg:leading-snug home-name-smaller  "> & Plant Enthusiast</h5>
+            </section>
+            
+        </main>
+
+
+
+
+
+    )
        
 }
